360acd4791e776373c789e11c0d2d04dcce153ed
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-17 12:48:37 +0100

    Added Calculator class and a test for Add

---
+++ TravisTestSolution/TravisTestSolution/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TravisTestSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TravisTestSolution/TravisTestSolutionUnitTest/CalculatorTests.cs
@@ -0,0 +1,17 @@
+using System;
+using TravisTestSolution;
+using Xunit;
+
+namespace TravisTestSolutionUnitTest
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_Add2Plus3_Returns5()
+        {
+            var uut = new Calculator();
+
+            Assert.Equal(5, uut.Add(2,3));
+        }
+    }
+}
