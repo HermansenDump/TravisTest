0fd38e226ade655b89986f5bd48f660a73f70255
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-17 13:15:38 +0100

    Build should fail

--- TravisTestSolution/TravisTestSolution/Program.cs
+++ TravisTestSolution/TravisTestSolution/Program.cs
@@ -6,7 +6,7 @@ namespace TravisTestSolution
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(aaHello World!");
         }
     }
 }
