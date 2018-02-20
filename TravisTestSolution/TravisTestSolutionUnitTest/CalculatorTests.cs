b40aa6641eb92712ec2219011557b2fac84a7c3a
Kasper Juul Hermansen <hermansendesigns@gmail.com>
2018-02-20 10:42:38 +0100

    Build will fail

--- TravisTestSolution/TravisTestSolutionUnitTest/CalculatorTests.cs
+++ TravisTestSolution/TravisTestSolutionUnitTest/CalculatorTests.cs
@@ -13,5 +13,13 @@ namespace TravisTestSolutionUnitTest
 
             Assert.Equal(5, uut.Add(2,3));
         }
+
+        [Fact]
+        public void Add_Add3Plus3_Returns5()
+        {
+            var uut = new Calculator();
+
+            Assert.Equal(3, uut.Add(2, 3));
+        }
     }
 }
